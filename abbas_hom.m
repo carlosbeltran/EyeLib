3e60c552ad194d0f0795f4186025e98f92fa9b1c
carlitos <cbeltran@libero.it>
2020-04-28 22:01:44 +0200

    Introduced new synthetic code with Peter Corke toolboxes dipences
    Slowly starting to understand angles conventions in the original Abbas CARLA-VP dataset

--- abbas_hom.m
+++ abbas_hom.m
@@ -9,7 +9,7 @@ K_gt = [[403.76110881   0. 288.        ]; ...
  [  0.    403.76110881 216.        ]; ...
  [  0.       0.           1.        ]];
 fov_gt = 71.0;
-camera_height = 7.692022323608398
+camera_height = 7.692022323608398;
 pitch_gt = -16.022058486938477;
 yaw_gt = 24.999956130981445;
 roll_gt = -5.862370014190674;
@@ -30,45 +30,60 @@ image_height = 432;
 % image_width = 540;
 % image_height = 432;
 
-thetax = deg2rad(pitch_gt);
-%thetax = deg2rad(roll_gt);
-Rx_gt = [ 1  0 0;
-    0 cos(thetax) -sin(thetax);
-    0 sin(thetax) cos(thetax)];
+%Rot_gt = rpy2r(roll_gt,pitch_gt,yaw_gt,'deg','camera');
+Rot_gt = rpy2r(roll_gt,pitch_gt,yaw_gt,'deg','yxz');
+Rot_gt_roll_pitch = rpy2r(roll_gt,pitch_gt,0,'deg','yxz')
+Rot_gt_inv = Rot_gt';
 
-thetay = deg2rad(yaw_gt);
-%thetay = deg2rad(pitch_gt);
-Ry_gt = [cos(thetay) 0 sin(thetay);
-    0 1 0;
-    -sin(thetay) 0 cos(thetay)];
+% % % %thetax = deg2rad(pitch_gt);
+% % % 
+% % % thetax = deg2rad(roll_gt);
+% % % Rx_gt = [ 1  0 0;
+% % %     0 cos(thetax) -sin(thetax);
+% % %     0 sin(thetax) cos(thetax)];
+% % % 
+% % % %thetay = deg2rad(yaw_gt);
+% % % thetay = deg2rad(pitch_gt);
+% % % Ry_gt = [cos(thetay) 0 sin(thetay);
+% % %     0 1 0;
+% % %     -sin(thetay) 0 cos(thetay)];
+% % % 
+% % % %thetaz = deg2rad(roll_gt);
+% % % thetaz = deg2rad(yaw_gt);
+% % % Rz_gt = [ cos(thetaz) -sin(thetaz) 0;
+% % %     sin(thetaz) cos(thetaz) 0;
+% % %     0 0 1];
+% % % 
+% % % Rot_gt = Rz_gt* Ry_gt * Rx_gt;
+% % % %Rot_gt = Rx_gt * Ry_gt * Ry_gt;
+% % % %Rot_gt = Rz_gt * Rx_gt;
 
-thetaz = deg2rad(roll_gt);
-%thetaz = deg2rad(yaw_gt);
-Rz_gt = [ cos(thetaz) -sin(thetaz) 0;
-    sin(thetaz) cos(thetaz) 0;
-    0 0 1];
-
-Rot_gt = Rz_gt* Ry_gt * Rx_gt;
-%Rot_gt = Rz_gt * Rx_gt;
+%check vanishing points from rotation matrix
+vvv1 = K_gt*Rot_gt_inv(:,1);
+vvv1 = vvv1/vvv1(3)
+vvv2 = K_gt*Rot_gt_inv(:,2);
+vvv2 = vvv2/vvv2(3)
+vvv3 = K_gt*Rot_gt_inv(:,3);
+vvv3 = vvv3/vvv3(3)
 
 % open image
 I = imread('town_1_frame_1268.png');
 %I = imread('town_1_frame_2098.png');
-imshow(I);
-hold on
+% imshow(I);
+% hold on
 
 % Plot vanishing points
-plot(vps_gt(1,1),vps_gt(1,2),'*r');
-plot(vps_gt(2,1),vps_gt(2,2),'*g');
-plot(vps_gt(3,1),vps_gt(3,2),'*b');
+% plot(vps_gt(1,1),vps_gt(1,2),'*r');
+% plot(vps_gt(2,1),vps_gt(2,2),'*g');
+% plot(vps_gt(3,1),vps_gt(3,2),'*b');
 
 % compute horizon line
 horizon = cross([vps_gt(1,:),1],[vps_gt(2,:),1]);
 
 %computer roll around z axis
 theta_z = atan(-horizon(1)/horizon(2));
-roll_deg = rad2deg(theta_z)
-roll_gt
+roll_deg = rad2deg(theta_z);
+roll_gt;
 
 %Rotation matrix on z axis
 Rz = [ cos(theta_z) -sin(theta_z) 0;
@@ -78,8 +93,8 @@ Rz = [ cos(theta_z) -sin(theta_z) 0;
 % compute x rotation (tilt) [ this doesn't seem to work, error in stimated
 % angle ]
 theta_x = (pi/2.0) - atan(norm([vps_gt(3,:)])/K_gt(1));
-tild_deg = rad2deg(theta_x)
-pitch_gt
+tild_deg = rad2deg(theta_x);
+pitch_gt;
 
 % instead trying to get the pitch angle from the Image of The Absolute Conic (IAC)
 % using the camera center vector and the vertical vanishing point (vps3_gt)
@@ -89,7 +104,7 @@ x1  = K_gt(:,3); % Image centre (first image point)
 iac = inv(K_gt*K_gt'); %Image of the absolute conic
 x2  = [vps_gt(3,:),1]'; % Vanishing point (second image point)
 costheta = (x1'*iac*x2)/(sqrt(x1'*iac*x1)*sqrt(x2'*iac*x2));
-pitch_iac = rad2deg(pi/2 - acos(costheta))
+pitch_iac = rad2deg(pi/2 - acos(costheta));
 
 theta_x = pi/2 - acos(costheta);
 
@@ -97,26 +112,28 @@ Rx = [ 1  0 0;
     0 cos(theta_x) -sin(theta_x);
     0 sin(theta_x) cos(theta_x)];
 
-RotM = Rz*Rx
+RotM = Rx*Rz
 
 % Compute directly rotation matrix columns
 % It doesn't seem to work
 % wrongly compute vanishing points?
-r1 = inv(K_gt)*[vps_gt(1,:),1]'/norm(inv(K_gt)*[vps_gt(1,:),1]'); 
-r2 = inv(K_gt)*[vps_gt(2,:),1]'/norm(inv(K_gt)*[vps_gt(2,:),1]');
+r1 = (inv(K_gt)*[vps_gt(1,:),1]')/norm(inv(K_gt)*[vps_gt(1,:),1]'); 
+r2 = (inv(K_gt)*[vps_gt(2,:),1]')/norm(inv(K_gt)*[vps_gt(2,:),1]');
 r3 = cross(r1,r2);
 RotM_2 = [r1,r2,r3]
 
 Rot_gt
+Rot_gt_roll_pitch
 
-% %Compute Homography??
-%Hrot = K_gt*inv(Rx_gt)*inv(K_gt)*inv(Rz_gt);
-Hrot = K_gt*Rx*inv(K_gt)*Rz;
 
-%Hrot = inv(Rz_gt) * inv(Rx_gt);
-
-% % apply rotation to the image plane
-tform = projective2d(Hrot');
-imout = imwarp(I,tform);
-figure;
-imshow(imout);
+% % % %Compute Homography??
+% % %Hrot = K_gt*inv(Rx_gt)*inv(K_gt)*inv(Rz_gt);
+% % Hrot = K_gt*Rx*inv(K_gt)*Rz;
+% % 
+% % %Hrot = inv(Rz_gt) * inv(Rx_gt);
+% % 
+% % % % apply rotation to the image plane
+% % tform = projective2d(Hrot');
+% % imout = imwarp(I,tform);
+% % figure;
+% % imshow(imout);
---
+++ vpinvestigation.m
@@ -0,0 +1,65 @@
+clear all; close all;
+
+% Plot word coordinate frame
+Tcam = transl(0,0,1)*trotx(deg2rad(-90))*troty(deg2rad(45))*trotx(deg2rad(-45));
+
+cam = CentralCamera('focal', 0.015,'pixel',10e-6, ...
+     'resolution',[1280 1024],'centre',[640 512],'pose',Tcam);
+
+world = SE3();
+trplot(world,'frame','0','color','b');
+
+
+
+% plot camera
+cam.plot_camera();
+
+% Plot points
+hold on;
+P = mkgrid(2,0.2,'pose',transl(0.7,0.7,0));
+plot3(P(1,:),P(2,:),P(3,:));
+%figure;
+
+cam.plot(P);
+xy = cam.project(P);
+xx = xy(1,:);
+yy = xy(2,:);
+
+figure;
+set(gca,'Ydir','reverse')
+p1 = [xx(1),yy(1)];
+p2 = [xx(2),yy(2)];
+p3 = [xx(3),yy(3)];
+p4 = [xx(4),yy(4)];
+hold on
+plot(p1(1),p1(2),'*r');
+plot(p2(1),p2(2),'*r');
+plot(p3(1),p3(2),'*y');
+plot(p4(1),p4(2),'*y');
+
+line1 = cross([p1,1],[p2,1]);
+plot(xx(1:2),yy(1:2),'-r');
+line2 = cross([p4,1],[p3,1]);
+plot([xx(4);xx(3)],[yy(4);yy(3)],'-r');
+
+line3 = cross([p4,1],[p1,1]);
+plot([xx(4);xx(1)],[yy(4);yy(1)],'-y');
+line4 = cross([p3,1],[p2,1]);
+plot([xx(3);xx(2)],[yy(3);yy(2)],'-y');
+
+vp1 = cross(line1,line2);
+vp2 = cross(line3,line4);
+vp1 = vp1/vp1(3)
+vp2 = vp2/vp2(3)
+plot(vp1(1)/vp1(3),vp1(2)/vp1(3),'*r');
+plot(vp2(1)/vp2(3),vp2(2)/vp2(3),'*y');
+
+rot = cam.T.R;
+rot = rot';
+vv1 = cam.K * rot(:,1);
+vv1 = vv1/vv1(3)
+vv2 = cam.K * rot(:,2);
+vv2 = vv2/vv2(3)
+rot
+r1 = (inv(cam.K)*vp1')/norm((inv(cam.K)*vp1'))
+r2 = (inv(cam.K)*vp2')/norm((inv(cam.K)*vp2'))
