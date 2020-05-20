03dc89b334518e3cae8f6cd3560710db6bf3feda
carlitos <cbeltran@libero.it>
2020-05-20 18:48:35 +0200

    Trying to implement Criminisi 1999-2000

---
+++ camheightCriminisi.m
@@ -0,0 +1,50 @@
+clear all; close all;
+
+% Plot word coordinate frame
+Tcam = transl(0,0,4)*trotx(deg2rad(-90))*troty(deg2rad(45))*trotx(deg2rad(-35));
+
+cam = CentralCamera('focal', 0.015,'pixel',10e-6, ...
+     'resolution',[1280 1024],'centre',[640 512],'pose',Tcam);
+
+world = SE3();
+trplot(world,'frame','0','color','b');
+
+xlim([-1 5]);
+ylim([-1 5]);
+zlim([-0.1 5]);
+
+% plot camera
+cam.plot_camera();
+
+% Plot points
+hold on;
+%%P = mkgrid(2,0.2,'pose',transl(0.7,0.7,0));
+%P = mkcube(1.8,'pose',transl([4,4,0.9]));
+%P = P(:,[1,5]);
+P = [ 4 4; 4 4; 0 1.8];
+plot3(P(1,:),P(2,:),P(3,:),'LineWidth',7); 
+cam.plot(P);
+
+B = P(:,1);
+T = P(:,2);
+
+xy = cam.project(P);
+
+b = [xy(:,1);1];
+t = [xy(:,2);1];
+ 
+R = cam.T.R;
+R = R';
+K = cam.K
+[v1,v2,v3] = computeVanishingPointsviaRandK(R,K);
+
+hoz    = cross(v1,v2);
+btLine = cross(b,t);
+i      = cross(btLine,hoz);
+i = i/i(3);
+
+% doesn't work yet
+hoznor = hoz/norm(hoz);
+Zt = 1.8;
+alpha = (-norm(cross(b,t)))/dot(hoznor,b)*norm(cross(v3,t))*Zt
+Zc    = (-norm(cross(b,i)))/dot(hoznor,b)*norm(cross(v3,i))*alpha
