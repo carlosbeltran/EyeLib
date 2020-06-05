ad234e0ab937e561126c0d52e5ad6706c8a261c0
carlitos <cbeltran@libero.it>
2020-06-05 17:29:57 +0200

    Just avoiding printing of v values

--- matlab/computeVanishingPointsviaRandK.m
+++ matlab/computeVanishingPointsviaRandK.m
@@ -14,11 +14,11 @@
 function [v1,v2,v3] = computeVanishingPointsviaRandK(R,K)
 
 v1 = K * R(:,1);
-v1 = v1/v1(3)
+v1 = v1/v1(3);
 v2 = K * R(:,2);
-v2 = v2/v2(3)
+v2 = v2/v2(3);
 v3 = K * R(:,3);
-v3 = v3/v3(3)
+v3 = v3/v3(3);
 
 end
 
